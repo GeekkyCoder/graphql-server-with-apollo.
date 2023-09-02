e90c1a9795430c692620c5324f688711ecae2d1c
Faraz <farazatgithub@gmail.com>
2023-09-02 12:18:23 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const express = require("express");
+
+const {ApolloServer} = require("apollo-server-express")
+
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+// const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const typesArray = loadFilesSync('**/*', {
+  extensions: ['graphql'],
+})
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+
+
+
+async function startServerWithApollo() {
+
+  const app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+
+  const server = new ApolloServer({
+    schema
+  })
+
+  await server.start()
+  server.applyMiddleware({app,path:"/graphql"})
+
+  app.listen(3000, () => {
+    console.log("Running GraphQL server...");
+  });
+  
+}
+
+startServerWithApollo()
+
